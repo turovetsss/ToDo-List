62b0c65a504d82e2a29a88cbb853150b178982a6
Barkovskiy Valeriy <VBarkovskiy@alfabank.ru>
2023-11-26 15:04:30 +0300

    new feature (routing)

---
+++ src/routes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ButtonsPage } from 'pages';
+
+export const ROUTES = [
+    {
+        path: '/buttons',
+        element: <ButtonsPage />,
+        errorElement: <>404</>,
+    },
+];
