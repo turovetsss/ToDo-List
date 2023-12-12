7ebed5fa05c89745955a6a17eaa67d9fd63f0954
vichklu <119040501+vichklu@users.noreply.github.com>
2023-12-12 22:32:33 +0300

    classname

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,19 +1,28 @@
 import React, { useMemo } from 'react';
+import { classname } from 'utils';
 
 import './input.scss';
+const cn = classname('input');
 
 export const Input = props => {
     const { type = 'input', disabled, view = 'filled', invalid, name, value, onChange, hint } = props;
 
-    const className = useMemo(() => ['input', `input--${view}`, invalid ? 'input--error' : '', props.className].join(' '), [invalid, props.className, view]);
+    const className = useMemo(() => {
+        const modifiers = {
+            [view]: view,
+            'input--error': invalid,
+        };
+
+        return cn(null, modifiers, props.className);
+    }, [invalid, props.className, view]);
 
     return (
         <div className={className}>
-            <label className='input__label'>Label</label>
-            <div className='input__left-addons'></div>
-            <input className='input__control' type={type} disabled={disabled} placeholder='Placeholder' name={name} value={value} onChange={onChange} />
-            <div className='input__right-addons'></div>
-            {hint && <span className='input__hint'>{hint}</span>}
+            <label className={cn('label')}>Label</label>
+            <div className={cn('left-addons')}></div>
+            <input className={cn('control')} type={type} disabled={disabled} placeholder='Placeholder' name={name} value={value} onChange={onChange} />
+            <div className={cn('right-addons')}></div>
+            {hint && <span className={cn('hint')}>{hint}</span>}
         </div>
     );
 };
