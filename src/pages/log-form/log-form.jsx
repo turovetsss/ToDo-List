023b173d9f074947ac9ff67d9eb303619ec1d5ff
vichklu <119040501+vichklu@users.noreply.github.com>
2023-12-29 02:43:46 +0300

    style lofin page

--- src/pages/log-form/log-form.jsx
+++ src/pages/log-form/log-form.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import './log-form.scss';
-
 export const LoginPage = () => {
     return (
-        <>
-            <header className='header'>
+        <div>
+            <div className='header'>
                 <div className='header__logo'>
-                    <img src='логотип.png' alt='Логотип' />
-                    <a href=''>Untlined</a>
+                    <img src={require('./img/logformimg.JPG')} height={32} />
+                    <img src={require('./img/Logotype.jpg')} height={30} width={100} />
                 </div>
                 <nav className='header__menu'>
                     <ul className='menu__list'>
@@ -26,11 +25,49 @@ export const LoginPage = () => {
                         </li>
                     </ul>
                 </nav>
+
                 <div className='header__account'>
                     <button className='login__button'>Log in</button>
                     <button className='signup__button'>Sign up</button>
                 </div>
-            </header>
-        </>
+            </div>
+            <form>
+                <div className='box'>
+                    <div className='box-signup'>
+                        <img src={require('./img/logformimg.JPG')} height={48} />
+                    </div>
+                    <h1>Log in to your account</h1>
+                    <p>Welcome back! Please enter your details.</p>
+                    <div className='text-box'>
+                        <label>Email</label>
+                        <input type='email' className='email' placeholder='Enter your email' />
+                    </div>
+                    <div className='text-box'>
+                        <label>Password</label>
+                        <input type='password' className='email' />
+                    </div>
+                    <div className='check-box'>
+                        <div>
+                            <input type='checkbox' id='scales' name='scales' checked={false} />
+                            <label htmlFor='scales'>Remember for 30 days</label>
+                        </div>
+                        <div className='rem'>
+                            <a className='a-name'>Forgot password</a>
+                        </div>
+                    </div>
+
+                    <div className='sign-btn'>Sign In</div>
+
+                    <div className='google-btn'>
+                        <img src={require('./img/soc.jpg')} height={24} />
+                        Sign in with Google
+                    </div>
+                    <div className='box-signup'>
+                        <a>Don’t have an account? </a>
+                        <a className='a-name'>Sign up</a>
+                    </div>
+                </div>
+            </form>
+        </div>
     );
 };
