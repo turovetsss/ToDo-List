461f737b9b526ff890046f522966ca2db4fe667a
vichklu <119040501+vichklu@users.noreply.github.com>
2024-02-24 13:03:48 +0300

    page btn

--- src/pages/table/table.jsx
+++ src/pages/table/table.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import down from 'pages/img/arrow-down.png';
+import left from 'pages/img/arrow-left.png';
+import right from 'pages/img/arrow-right.png';
 import avatar from 'pages/img/Avatar.png';
 import edit from 'pages/img/edit-2.png';
 import help from 'pages/img/Help-icon.png';
@@ -167,6 +169,25 @@ export const TablePage = () => {
                     </tr>
                 </tbody>
             </table>
+            <div className='page-btn'>
+                <div className='page-btn__previous'>
+                    <img src={left} alt='' />
+                    <div>Previous</div>
+                </div>
+                <div className='page-btn__num'>
+                    <div className='page-btn__num__btn'>1</div>
+                    <div className='page-btn__num__btn'>2</div>
+                    <div className='page-btn__num__btn'>3</div>
+                    <div className='page-btn__num__btn'>...</div>
+                    <div className='page-btn__num__btn'>8</div>
+                    <div className='page-btn__num__btn'>9</div>
+                    <div className='page-btn__num__btn'>10</div>
+                </div>
+                <div className='page-btn__next'>
+                    <div>Next</div>
+                    <img src={right} alt='' />
+                </div>
+            </div>
         </div>
     );
 };
