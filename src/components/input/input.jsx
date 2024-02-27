498a959a34de0751295d704d7d8657f454d49fcb
vichklu <119040501+vichklu@users.noreply.github.com>
2024-02-27 23:59:34 +0300

    view for input

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -6,21 +6,21 @@ const cn = classname('input');
 
 export const Input = props => {
     const { type = 'input', disabled, view = 'filled', invalid, name, value, onChange, hint } = props;
-
-    const className = useMemo(() => {
-        const modifiers = {
-            [view]: view,
-            'input--error': invalid,
-        };
-
-        return cn(null, modifiers, props.className);
-    }, [invalid, props.className, view]);
+    const className = useMemo(() => ['input', `input--${view}`, invalid ? 'input--error' : '', props.className].join(' '), [invalid, props.className, view]);
 
     return (
         <div className={className}>
             <label className={cn('label')}>Label</label>
             <div className={cn('left-addons')}></div>
-            <input className={cn('control')} type={type} disabled={disabled} placeholder='Placeholder' name={name} value={value} onChange={onChange} />
+            <input
+                className={cn('control', { view })}
+                type={type}
+                disabled={disabled}
+                placeholder='Placeholder'
+                name={name}
+                value={value}
+                onChange={onChange}
+            />
             <div className={cn('right-addons')}></div>
             {hint && <span className={cn('hint')}>{hint}</span>}
         </div>
--- src/pages/inputs/inputs.jsx
+++ src/pages/inputs/inputs.jsx
@@ -8,7 +8,7 @@ export const InputsPage = () => {
         <>
             <h4>filled</h4>
             <div className='inputs-container'>
-                <Input view='filled' className='input--normal' />
+                <Input view='filled' className='input--filled' />
                 <Input view='filled' className='input--focus' />
                 <Input view='filled' className='input--hover' />
                 <Input view='filled' invalid={true} className='input--error' />
@@ -16,58 +16,58 @@ export const InputsPage = () => {
             </div>
             <h4>Outlined</h4>
             <div className='inputs-container'>
-                <Input view='outlined' className='input--normal' />
-                <Input view='outlined' className='input--focus' />
-                <Input view='outlined' className='input--hover' />
-                <Input view='outlined' invalid={true} className='input--error' />
-                <Input view='outlined' disabled={true} />
+                <Input view='filled' className='input--filled input--search input--cross' />
+                <Input view='filled' className='input--focus input--search' />
+                <Input view='filled' className='input--hover input--search' />
+                <Input view='filled' invalid={true} className='input--error ' />
+                <Input view='filled' disabled={true} />
             </div>
             <h4>None</h4>
             <div className='inputs-container'>
-                <Input view='none' className='input--normal' />
-                <Input view='none' className='input--focus' />
-                <Input view='none' className='input--hover' />
-                <Input view='none' invalid={true} className='input--error' />
-                <Input view='none' disabled={true} />
+                <Input view='filled' className='input--normal  input--none' />
+                <Input view='filled' className='input--focus  input--none' />
+                <Input view='filled' className='input--hover  input--none' />
+                <Input view='filled' invalid={true} className='input--error' />
+                <Input view='filled' disabled={true} />
             </div>
             <h4>Search input</h4>
             <div className='inputs-container'>
-                <Input view='search' className='input--normal' />
-                <Input view='search' className='input--focus' />
-                <Input view='search' className='input--hover' />
-                <Input view='search' invalid={true} className='input--error' />
-                <Input view='search' disabled={true} />
+                <Input view='filled' className='input--normal' />
+                <Input view='filled' className='input--focus' />
+                <Input view='filled' className='input--hover' />
+                <Input view='filled' invalid={true} className='input--error' />
+                <Input view='filled' disabled={true} />
             </div>
             <h4>With Border input</h4>
             <div className='inputs-container'>
-                <Input view='border' className='input--border-normal' />
-                <Input view='border' className='input--border input-focus' />
-                <Input view='border' className='input--border-input-hover' />
-                <Input view='border' invalid={true} className='input--error' />
-                <Input view='border' disabled={true} />
+                <Input view='border' className='input--view-border' />
+                <Input view='border' className='input--focus' />
+                <Input view='border' className='input--hover' />
+                <Input view='border' invalid={true} className='border--error' />
+                <Input view='border' disabled={true} className='input--disabled' />
             </div>
             <h4>Filled Border input</h4>
             <div className='inputs-container'>
-                <Input view='border' className='input--border-normal input--search' />
-                <Input view='border' className='input--border input-focus input--search' />
-                <Input view='border' className='input--border input-hover input--search' />
-                <Input view='border' invalid={true} className='input--error input--search ' />
+                <Input view='border' className='input--search' />
+                <Input view='border' className='input--focus input--search' />
+                <Input view='border' className='input--hover input--search' />
+                <Input view='border' invalid={true} className='input--error ' />
                 <Input view='border' className='input--search' disabled={true} />
             </div>
             <h4>None Border input</h4>
             <div className='inputs-container'>
-                <Input view='border' className='input--border-normal input--none' />
-                <Input view='border' className='input--border input-focus input--none' />
-                <Input view='border' className='input--border input-hover input--none' />
-                <Input view='border' invalid={true} className='input--error input--none' />
+                <Input view='border' className='input--none' />
+                <Input view='border' className='input--focus input--none' />
+                <Input view='border' className='input--hover input--none' />
+                <Input view='border' invalid={true} className='input--error' />
                 <Input view='border' disabled={true} className='input--none' />
             </div>
             <h4>Search Border input</h4>
             <div className='inputs-container'>
                 <Input view='border' className='input--border-normal input--search' />
-                <Input view='border' className='input--border input-focus input--search' />
-                <Input view='border' className='input--border input-hover input--search' />
-                <Input view='border' invalid={true} className='input--error input--search' />
+                <Input view='border' className='input--focus input--search' />
+                <Input view='border' className='input--hover input--search' />
+                <Input view='border' invalid={true} className='input--error' />
                 <Input view='border' className='input--search' disabled={true} />
             </div>
         </>
